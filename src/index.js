8b533dd009bf4fe31b696bbc98c0328b70036b6f
donavon <github@donavon.com>
2019-02-07 14:26:52 -0500

    added useEventListener

--- src/index.js
+++ src/index.js
@@ -4,6 +4,26 @@ import {
 
 import createGlobalState from './createGlobalState';
 
+const useEventListener = (eventName, handler) => {
+  const savedHandler = useRef();
+
+  useEffect(() => {
+    savedHandler.current = handler;
+  });
+
+  useEffect(
+    () => {
+      const eventListener = () => savedHandler.current();
+
+      global.addEventListener(eventName, eventListener);
+      return () => {
+        global.removeEventListener(eventName, eventListener);
+      };
+    },
+    [eventName]
+  );
+};
+
 const createStorage = provider => ({
   get(key, defaultValue) {
     const json = provider.getItem(key);
@@ -29,27 +49,23 @@ const createPersistedState = (key, provider = global.localStorage) => (initialSt
   const [state, setState] = useState(() => get(key, initialState));
 
   // only called on mount/unmount
-  useEffect(
-    () => {
-      // Listen for `localStorage` changes from other windows
-      const onStorage = ({ key: k, newValue }) => {
-        const newState = JSON.parse(newValue);
-        if (k === key && state !== newState) {
-          setState(newState);
-        }
-      };
-      global.addEventListener('storage', onStorage);
+  // subscribe to `storage` change events
+  useEventListener('storage', ({ key: k, newValue }) => {
+    const newState = JSON.parse(newValue);
+    if (k === key && state !== newState) {
+      setState(newState);
+    }
+  });
 
-      // register a listener that calls `setState` when another instance emits
-      globalState.current = createGlobalState(key, setState, state);
+  // only called on mount/unmount
+  useEffect(() => {
+    // register a listener that calls `setState` when another instance emits
+    globalState.current = createGlobalState(key, setState, state);
 
-      return () => {
-        global.removeEventListener('storage', onStorage);
-        globalState.current.deregister();
-      };
-    },
-    [key, provider]
-  );
+    return () => {
+      globalState.current.deregister();
+    };
+  }, []);
 
   // Only persist to storage if state changes.
   useEffect(
