c86acba864a123dd104e04e34fabe182bb7a47f2
Mark Adamson <mark@adamsonfamily.co.uk>
2019-11-13 20:56:12 +0000

    fix missing useCallback import

--- src/usePersistedState.js
+++ src/usePersistedState.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useCallback } from 'react';
 import useEventListener from '@use-it/event-listener';
 
 import createGlobalState from './createGlobalState';
