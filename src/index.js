49451ac65816e45ae995ca36284fcf8777c8135f
Vitor Dino <vitor@kunstdesign.com.br>
2020-11-19 17:18:03 -0300

    fix(provider): test-check if each provider exists (#24)
    this is breaking my app (running on gatsby) and probably this would be sufficient to fix it 👐 
    
    but i’ll have to test it first

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import { useState } from 'react';
 import usePersistedState from './usePersistedState';
 import createStorage from './createStorage';
 
+const getProvider = () => {
+	if(typeof global !== 'undefined') return global.localStorage
+	if(typeof globalThis !== 'undefined') return globalThis.localStorage
+	if(typeof window !== 'undefined') return window.localStorage
+	if(typeof localStorage !== 'undefined') return localStorage
+	return null
+}
+
 const createPersistedState = (
   key,
-  provider = global.localStorage
+  provider = getProvider()
 ) => {
   if (provider) {
     const storage = createStorage(provider);
