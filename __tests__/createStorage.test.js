8ef8c96f6d386589eaf209a7eb6f45ba5ea19673
Vadym Kalinin <vadym.kalinin@sinnerschrader.com>
2021-03-29 19:39:13 +0200

    feat(createStorage): add reviver option to the JSON.parse call

--- __tests__/createStorage.test.js
+++ __tests__/createStorage.test.js
@@ -69,4 +69,33 @@ describe('createStorage', () => {
       expect(get('key')).toBe(undefined);
     });
   });
+
+  describe('parseReviver', () => {
+    test('format specific values by key', () => {
+      const mockProvider = new Provider();
+      const { get, set } = createStorage(mockProvider, {
+        parseReviver: (key, value) => (key === 'Alice' ? value * 2 : value),
+      });
+
+      set('people', {
+        Ben: 41,
+        Alice: 24,
+      });
+      expect(get('people')).toEqual({
+        Ben: 41,
+        Alice: 48,
+      });
+    });
+
+    test('format to Date from string', () => {
+      const mockProvider = new Provider();
+      const { get, set } = createStorage(mockProvider, {
+        parseReviver: (_key, value) => new Date(value),
+      });
+
+      set('today', new Date(2019, 12, 31));
+      expect(get('today')).toBeInstanceOf(Date);
+      expect(get('today')).toEqual(new Date(2019, 12, 31));
+    });
+  });
 });
